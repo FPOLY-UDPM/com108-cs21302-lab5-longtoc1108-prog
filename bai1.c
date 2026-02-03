7d1782c11fec3d29359da809ca3265484d90f00b
longtoc1108-prog <crynotsadpvp@gmail.com>
2026-02-02 20:42:35 -0700

    Huy nộp bài

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [TRẦN VĂN HUY]
+ * MSSV:      [PS48688]
+ * Lớp:       [COM108-CS21302]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÌM GIÁ TRỊ LỚN NHẤT TRONG 3 SỐ  
@@ -11,19 +11,29 @@
 //  giá trị lớn nhất giữa 3 số 
 
 #include <stdio.h>
-
-// //Tạo hàm 
-// int tenHam(so1, so2, so3){ 
-// int max; 
-// //Cách giải để tìm ra số lớn nhất 
-// return max; 
-// } 
-
-int main() {
-
-    // //Gọi hàm trong hàm main 
-    // //ví dụ 
-    // int soMax = temHam(3,5,10)
-    
-    return 0;
+float Max3(float a, float b, float c) {
+    float max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    printf("Số lớn nhất trong 3 số %.2f, %.2f, %.2f là: %.2f\n", a, b, c, max);
+    return max;
 }
+float Min3(float a, float b, float c) {
+    float min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    printf("Số nhỏ nhất trong 3 số %.2f, %.2f, %.2f là: %.2f\n", a, b, c, min);
+    return min;
+}
+int main() {
+    float a, b, c;
+    printf("Nhập số thứ nhất: ");
+    scanf("%f", &a);
+    printf("Nhập số thứ hai: ");
+    scanf("%f", &b);
+    printf("Nhập số thứ ba: ");
+    scanf("%f", &c);
+    Max3(a, b, c);
+    Min3(a, b, c);
+    return 0;
+}    
